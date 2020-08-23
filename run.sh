3372eaa1e642e47df16f1480c31e214d6e200b83
Sid <sidharthsp@gmail.com>
2020-08-23 11:15:22 -0400

    requirements changed

--- run.sh
+++ run.sh
@@ -7,3 +7,4 @@ mkdir ~/venv
 python3.7 -m venv ~/venv
 source ~/venv/bin/activate
 pip install -r ./requirements.txt
+#pip install torch==1.5.1+cu101 torchvision==0.6.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
